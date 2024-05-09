0219433c60bcd8ac0dd3aecf427ea738360b180d
BREVIC3PS <kwang655@usc.edu>
2024-05-08 21:42:06 -0700

    1111

--- Project/test.sh
+++ Project/test.sh
@@ -2,21 +2,17 @@ mkdir -p Result
 
 for i in {1..15}
 do
-  # 构造输入文件名和输出文件名
   input_file="./datapoints/in${i}.txt"
   output_file="./Result/Ouroutput${i}_basic.txt"
 
-  # 执行 basic.sh 脚本
   ./basic.sh $input_file $output_file
 done
 
 for i in {1..15}
 do
-  # 构造输入文件名和输出文件名
   input_file="./datapoints/in${i}.txt"
   output_file="./Result/Ouroutput${i}_efficient.txt"
 
-  # 执行 efficient.sh 脚本
   ./efficient.sh $input_file $output_file
 done
 
